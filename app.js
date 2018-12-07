28770e5c87bf65bb74b4a17b65d6093bd53ecc38
Cory Morrissey <corydm92@gmail.com>
2018-12-07 12:42:39 -0800

    desktop transition menu button working

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ let menuButton = document.getElementById("menu-button");
 let navContainer = document.getElementById("nav-container");
 
 menuButton.addEventListener("click", function(e) {
-    if (navContainer.classList.contains("display-none")) {
-        navContainer.classList.add("inline-block");
-        navContainer.classList.remove("display-none");
-        // navContainer.style.visibility = "visible";
-    } else {
-        navContainer.classList.remove("inline-block");
-        navContainer.classList.add("display-none");
-        // navContainer.style.visibility = "hidden";
-    };    
 
     if (navContainer.classList.contains("unclicked")) {
         navContainer.classList.add("clicked");
