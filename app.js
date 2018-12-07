ac483054317f97b7059db9efb0f858cf797e4daf
Cory Morrissey <corydm92@gmail.com>
2018-12-07 14:18:53 -0800

    added transitions

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ let navContainer = document.getElementById("nav-container");
 menuButton.addEventListener("click", function(e) {
 
     if (navContainer.classList.contains("unclicked")) {
+        menuButton.classList.add("clicked")
         navContainer.classList.add("clicked");
         navContainer.classList.remove("unclicked")
         
     } else if (navContainer.classList.contains("clicked")) {
+        menuButton.classList.remove("clicked")
         navContainer.classList.remove("clicked");
         navContainer.classList.add("unclicked");
     };
+
+
 
 });
